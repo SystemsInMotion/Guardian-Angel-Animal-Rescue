5563e52c16cbe662afd435dc234c0c127e64a5e2
skronek <keithskronek@gmail.com>
2012-10-01 15:23:50 -0400

    updated POM and DefaultController to serve up home page

--- src/main/java/org/gaar/web/controller/DefaultController.java
+++ src/main/java/org/gaar/web/controller/DefaultController.java
@@ -1,20 +1,16 @@
 package org.gaar.web.controller;
 
+import org.gaar.web.View;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DefaultController {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String doDefault(Model model){
-		
-		String str = "something";
-		final byte[] bytes = str.getBytes();
-		DigestUtils.md5DigestAsHex(bytes);
-		return null;
+	@RequestMapping(method = RequestMethod.GET)
+	public String doHome(Model model) {
+		return View.home.name();
 	}
 }
