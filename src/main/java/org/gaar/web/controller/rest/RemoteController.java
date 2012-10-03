6e0a0601e7562035dc659a99a87350cd489f1255
skronek <keithskronek@gmail.com>
2012-10-03 01:00:16 -0400

    added doGetCats() and doGetDogs() in RemoteController and related test.  No real changes in DefaultControllerTest

--- src/main/java/org/gaar/web/controller/rest/RemoteController.java
+++ src/main/java/org/gaar/web/controller/rest/RemoteController.java
@@ -3,8 +3,11 @@ package org.gaar.web.controller.rest;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.gaar.web.service.consumer.PetFinderConsumer;
+import org.petfinder.entity.AnimalType;
 import org.petfinder.entity.PetfinderPetRecord;
 import org.petfinder.entity.PetfinderPetRecordList;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +20,58 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class RemoteController {
 
 	private static final Character ADOPTION_STATUS_NULL = null;
+	@SuppressWarnings("unused")
+	private static final String BREED_NULL = null;
 	private static final int RETURN_COUNT_ALL = 1000;
 	private static final Integer RECORD_OFFSET_NULL = null;
 	private static final String OUTPUT_NULL = null;
 	private static final String FORMAT_NULL = null;
 	private static final String SHELTER_ID_GAAR = "MI144";
-	
+
 	@Autowired
 	PetFinderConsumer petFinderService;
 
 	@RequestMapping(value = "pet/{petId}", method = GET)
 	public @ResponseBody
-	PetfinderPetRecord doGetPet(@PathVariable(value = "petId") int animalId) {
-		return petFinderService.getPet(BigInteger.valueOf(animalId), FORMAT_NULL);
+	PetfinderPetRecord doGetPet(@PathVariable(value = "petId") Integer id) {
+		return petFinderService.getPet(BigInteger.valueOf(id), FORMAT_NULL);
 	}
-	
+
 	@RequestMapping(value = "pets", method = GET)
-	public @ResponseBody PetfinderPetRecordList doGetPets() {
-		// TODO - check is cache is expired, if so refresh cache and return list from cache
-		PetfinderPetRecordList shelterPets = petFinderService.shelterPets(SHELTER_ID_GAAR, ADOPTION_STATUS_NULL, RECORD_OFFSET_NULL, RETURN_COUNT_ALL, OUTPUT_NULL, FORMAT_NULL);
-		return shelterPets;
+	public @ResponseBody
+	List<PetfinderPetRecord> doGetPets() {
+		// TODO - check is cache is expired, if so refresh cache and return list
+		// from cache
+		PetfinderPetRecordList shelterPets = petFinderService.shelterPets(SHELTER_ID_GAAR, ADOPTION_STATUS_NULL,
+				RECORD_OFFSET_NULL, RETURN_COUNT_ALL, OUTPUT_NULL, FORMAT_NULL);
+		final List<PetfinderPetRecord> pets = shelterPets.getPet();
+		return pets;
 	}
-	
+
+	@RequestMapping(value = "cats", method = GET)
+	public @ResponseBody
+	List<PetfinderPetRecord> doGetCats() {
+		final List<PetfinderPetRecord> pets = doGetPets();
+		List<PetfinderPetRecord> cats = new ArrayList<PetfinderPetRecord>();
+		for (PetfinderPetRecord pet : pets) {
+			if (pet.getAnimal().equals(AnimalType.CAT)) {
+				cats.add(pet);
+			}
+		}
+		return cats;
+	}
+
+	@RequestMapping(value = "dogs", method = GET)
+	public @ResponseBody
+	List<PetfinderPetRecord> doGetDogs() {
+		final List<PetfinderPetRecord> pets = doGetPets();
+		List<PetfinderPetRecord> dogs = new ArrayList<PetfinderPetRecord>();
+		for (PetfinderPetRecord pet : pets) {
+			if (pet.getAnimal().equals(AnimalType.DOG)) {
+				dogs.add(pet);
+			}
+		}
+		return dogs;
+	}
+
 }
--- src/test/java/org/gaar/web/controller/rest/RemoteControllerTest.java
+++ src/test/java/org/gaar/web/controller/rest/RemoteControllerTest.java
@@ -1,21 +1,16 @@
 package org.gaar.web.controller.rest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.math.BigInteger;
 import java.util.List;
 
-import org.gaar.web.controller.rest.RemoteController;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.MethodRule;
-import org.junit.rules.TestWatchman;
 import org.junit.runner.RunWith;
-import org.junit.runners.model.FrameworkMethod;
+import org.petfinder.entity.AnimalType;
 import org.petfinder.entity.PetfinderPetRecord;
-import org.petfinder.entity.PetfinderPetRecordList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -23,37 +18,43 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/rest-context.xml" })
 public class RemoteControllerTest {
-	
-	@SuppressWarnings("deprecation")
-	@Rule public MethodRule watchman = new TestWatchman() {
-	    public void starting(FrameworkMethod method) {
-	      logger.info("{} being run...", method.getName());
-	    }
-	  };
-	  
-	private static Logger logger = LoggerFactory.getLogger(RemoteControllerTest.class);
-	// static RequestSpecification req = given().contentType(ContentType.JSON);
-	// static ResponseSpecification res =
-	// expect().statusCode(200).contentType(ContentType.JSON);
 
 	@Autowired
 	private RemoteController remoteController;
 
-	@Before
-	public void before() {
-		// this.remoteController = new RemoteControllextr();
+	@Test
+	public void doGetPets() {
+		final List<PetfinderPetRecord> pets = this.remoteController.doGetPets();
+		assertNotNull(pets);
 	}
 
 	@Test
-	public void getPets() {
-		final PetfinderPetRecordList petRecordList = remoteController.doGetPets();
-		assertNotNull(petRecordList);
-		final List<PetfinderPetRecord> petRecords = petRecordList.getPet();
-		// final RequestSpecification reqSpec = given();
-		// final ResponseSpecification spec =
-		// given().spec(req).expect().spec(res);
-		// .body()
-		// .when().get(walletURL);
+	public void doGetPet() {
+		final List<PetfinderPetRecord> pets = this.remoteController.doGetPets();
+		assertNotNull(pets);
+		final PetfinderPetRecord pet = pets.get(0);
+		assertNotNull(pet);
+		final BigInteger id = pet.getId();
+		final PetfinderPetRecord petRecord = this.remoteController.doGetPet(id.intValue());
+		assertNotNull(petRecord);
+		assertTrue(id.equals(petRecord.getId()));
 	}
 
+	@Test
+	public void doGetCats() {
+		final List<PetfinderPetRecord> cats = this.remoteController.doGetCats();
+		assertNotNull(cats);
+		for (PetfinderPetRecord cat : cats) {
+			assertEquals(AnimalType.CAT, cat.getAnimal());
+		}
+	}
+
+	@Test
+	public void doGetDogs() {
+		final List<PetfinderPetRecord> dogs = this.remoteController.doGetDogs();
+		assertNotNull(dogs);
+		for (PetfinderPetRecord dog : dogs) {
+			assertEquals(AnimalType.DOG, dog.getAnimal());
+		}
+	}
 }
