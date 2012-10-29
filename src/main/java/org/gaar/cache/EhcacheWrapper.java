38623c0b45746acfcc61a82917de8781f80aeeaa
skronek <keithskronek@gmail.com>
2012-10-29 07:33:05 -0400

    Added logging and insured cache is created

--- src/main/java/org/gaar/cache/EhcacheWrapper.java
+++ src/main/java/org/gaar/cache/EhcacheWrapper.java
@@ -6,15 +6,18 @@ import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("cacheWrapper")
 @Scope("singleton")
 public class EhcacheWrapper<K, V> implements CacheWrapper<K, V> {
-
+	private static Logger logger = LoggerFactory.getLogger(EhcacheWrapper.class);
+	
 	private static final String CACHE_NAME = "petFinderCache";
-	private static final String EHCACHE_XML = "/ehcache.xml";
+	private static final String EHCACHE_XML = "classpath:ehcache.xml";
 	public static final String FULL_RESULT_KEY_SUFFIX = "_full";
 	public static final String PAGE_RESULT_KEY_SUFFIX = "_page";
 
@@ -23,6 +26,10 @@ public class EhcacheWrapper<K, V> implements CacheWrapper<K, V> {
 	public EhcacheWrapper() {
 		URL url = getClass().getResource(EHCACHE_XML);
 		cacheManager = new CacheManager(url);
+		if(!cacheManager.cacheExists(CACHE_NAME)){
+			cacheManager.addCache(CACHE_NAME);
+		}
+		logger.info(CACHE_NAME + " : " + this.cacheManager.getActiveConfigurationText(CACHE_NAME));
 	}
 
 	@SuppressWarnings("unchecked")
