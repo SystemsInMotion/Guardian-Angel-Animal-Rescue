4d0a6ebefa2b9fe88d6635ffeabf4197a68c0a07
skronek <keithskronek@gmail.com>
2012-11-05 11:31:34 -0500

    Added doMail to test sending email for adoption forms

--- src/main/java/org/gaar/web/controller/mvc/DefaultController.java
+++ src/main/java/org/gaar/web/controller/mvc/DefaultController.java
@@ -2,6 +2,14 @@ package org.gaar.web.controller.mvc;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.gaar.web.View;
 import org.gaar.web.service.consumer.PetFinderConsumer;
@@ -9,9 +17,13 @@ import org.petfinder.entity.PetfinderPetRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.systemsinmotion.petrescue.web.bean.AdoptionApplication;
 
 @Controller
 public class DefaultController {
@@ -60,6 +72,11 @@ public class DefaultController {
 	public String doAdoptions(Model model) {
 		return View.adoptions.name();
 	}
+	
+	@RequestMapping(value = "adopt", method = RequestMethod.POST)
+	public String doAdoptions_POST(@Validated AdoptionApplication application, Model model){
+		return  "redirect:/";
+	}
 
 	@RequestMapping(value = "contact", method = RequestMethod.GET)
 	public String doContact(Model model) {
@@ -79,5 +96,51 @@ public class DefaultController {
 	@RequestMapping(value = "volunteer", method = RequestMethod.GET)
 	public String doVolunteer(Model model) {
 		return View.volunteer.name();
+	}
+
+	@RequestMapping(value = "mail", method = RequestMethod.GET)
+	public @ResponseBody
+	String doMail() {
+		// Recipient's email ID needs to be mentioned.
+		String to = "keithskronek@gmail.com";
+
+		// Sender's email ID needs to be mentioned
+		String from = "keith@skronek.org";
+
+		// Assuming you are sending email from localhost
+		String host = "localhost";
+
+		// Get system properties
+		Properties properties = System.getProperties();
+
+		// Setup mail server
+		properties.setProperty("mail.smtp.host", host);
+
+		// Get the default Session object.
+		Session session = Session.getDefaultInstance(properties);
+
+		try {
+			// Create a default MimeMessage object.
+			MimeMessage message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			// Set Subject: header field
+			message.setSubject("This is the Subject Line!");
+
+			// Now set the actual message
+			message.setText("This is actual message");
+
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+		return "success";
 	}
 }
