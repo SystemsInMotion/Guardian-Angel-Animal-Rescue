06ba7d869186e1418423952b002cd9a25ab3f2f2
skronek <keithskronek@gmail.com>
2012-11-19 10:15:22 -0500

    new fields for adoption form.  May have reordered also

--- src/main/java/com/systemsinmotion/petrescue/web/bean/AdoptionApplication.java
+++ src/main/java/com/systemsinmotion/petrescue/web/bean/AdoptionApplication.java
@@ -3,145 +3,97 @@ package com.systemsinmotion.petrescue.web.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-//import javax.validation.constraints.NotNull;
+import com.systemsinmotion.util.OutputUtils;
+
 
 public class AdoptionApplication implements Serializable {
 
 	private static final long serialVersionUID = 1118000426493607246L;
 
-	private String firstName;
-
-	private String lastName;
-
-	private String address1;
-
-	private String address2;
-
-	private String zipCode;
-
-	private String city;
-
-	private String homePhone;
-	private String mobilePhone;
-	private String email1;
-	private String email2;
-	private String livingSituation;
-	private String yearsLived;
-	private String monthsLived;
+	private Boolean agreedToHomeVisit;
+	private Boolean awareOfUnknownIssues;
+	private Boolean familyAware;
+	private Boolean haveAppliedElsewhere;
 	private Boolean petsAllowed;
 	private Boolean petsAllowedProof;
+	private Boolean planToAttendObedienceClasses;
 	private Integer adultCount;
 	private Integer childCount;
-	private List<Integer> childAges;
-	private Boolean familyAware;
-	private String familyAwareNoReason;
-	private String caretaker;
+	private Integer currentPetCount;
+	private Integer hoursAlone;
+	private Integer householdMemberCount;
 	private Integer previousPetCount;
+	private List<Integer> childAges;
+	private List<Integer> householdMemberAges;
 	private List<PreviousPet> previousPets;
+	private List<Vet> vets;
+	private List<String> whyWantPet;
+	private String address1;
+	private String address2;
+	private String caretaker;
+	private String city;
+	private String daytimeLocation;
+	private String destructiveBehavior;
+	private String email;
+	private String emailConfirm;
+	private String failedAdoptionReason;
+	private String familyNotAwareReason;
+	private String fencedYard;
+	
+	@NotNull
+	private String firstName;
+	private String giveUpSituation;
+	private String homePhone;
+	private String howContainPet;
+	private String idealPet;
+	private String lastName;
+	private String livingSituation;
+	private String mobilePhone;
+	private String monthsLived;
+	private String movingPlan;
+	private String petLivingLocation;
+	private String planToDeclaw;
+	private String sleepLocation;
+	private String timeSearching;
+	private String whereApplied;
+	private String willingToInstallFence;
+	private String yearsLived;
+	private String zipCode;
 
-	public String getFirstName() {
-		return firstName;
+	public Boolean getAgreedToHomeVisit() {
+		return agreedToHomeVisit;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setAgreedToHomeVisit(Boolean agreedToHomeVisit) {
+		this.agreedToHomeVisit = agreedToHomeVisit;
 	}
 
-	public String getLastName() {
-		return lastName;
+	public Boolean getAwareOfUnknownIssues() {
+		return awareOfUnknownIssues;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setAwareOfUnknownIssues(Boolean awareOfUnknownIssues) {
+		this.awareOfUnknownIssues = awareOfUnknownIssues;
 	}
 
-	public String getAddress1() {
-		return address1;
+	public Boolean getFamilyAware() {
+		return familyAware;
 	}
 
-	public void setAddress1(String address1) {
-		this.address1 = address1;
+	public void setFamilyAware(Boolean familyAware) {
+		this.familyAware = familyAware;
 	}
 
-	public String getAddress2() {
-		return address2;
+	public Boolean getHaveAppliedElsewhere() {
+		return haveAppliedElsewhere;
 	}
 
-	public void setAddress2(String address2) {
-		this.address2 = address2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public String getHomePhone() {
-		return homePhone;
-	}
-
-	public void setHomePhone(String homePhone) {
-		this.homePhone = homePhone;
-	}
-
-	public String getMobilePhone() {
-		return mobilePhone;
-	}
-
-	public void setMobilePhone(String mobilePhone) {
-		this.mobilePhone = mobilePhone;
-	}
-
-	public String getEmail1() {
-		return email1;
-	}
-
-	public void setEmail1(String email1) {
-		this.email1 = email1;
-	}
-
-	public String getEmail2() {
-		return email2;
-	}
-
-	public void setEmail2(String email2) {
-		this.email2 = email2;
-	}
-
-	public String getLivingSituation() {
-		return livingSituation;
-	}
-
-	public void setLivingSituation(String livingSituation) {
-		this.livingSituation = livingSituation;
-	}
-
-	public String getYearsLived() {
-		return yearsLived;
-	}
-
-	public void setYearsLived(String yearsLived) {
-		this.yearsLived = yearsLived;
-	}
-
-	public String getMonthsLived() {
-		return monthsLived;
-	}
-
-	public void setMonthsLived(String monthsLived) {
-		this.monthsLived = monthsLived;
+	public void setHaveAppliedElsewhere(Boolean haveAppliedElsewhere) {
+		this.haveAppliedElsewhere = haveAppliedElsewhere;
 	}
 
 	public Boolean getPetsAllowed() {
@@ -160,28 +112,12 @@ public class AdoptionApplication implements Serializable {
 		this.petsAllowedProof = petsAllowedProof;
 	}
 
-	public Boolean getFamilyAware() {
-		return familyAware;
+	public Boolean getPlanToAttendObedienceClasses() {
+		return planToAttendObedienceClasses;
 	}
 
-	public void setFamilyAware(Boolean familyAware) {
-		this.familyAware = familyAware;
-	}
-
-	public String getFamilyAwareNoReason() {
-		return familyAwareNoReason;
-	}
-
-	public void setFamilyAwareNoReason(String familyAwareNoReason) {
-		this.familyAwareNoReason = familyAwareNoReason;
-	}
-
-	public Integer getPreviousPetCount() {
-		return previousPetCount;
-	}
-
-	public void setPreviousPetCount(Integer previousPetCount) {
-		this.previousPetCount = previousPetCount;
+	public void setPlanToAttendObedienceClasses(Boolean planToAttendObedienceClasses) {
+		this.planToAttendObedienceClasses = planToAttendObedienceClasses;
 	}
 
 	public Integer getAdultCount() {
@@ -200,12 +136,92 @@ public class AdoptionApplication implements Serializable {
 		this.childCount = childCount;
 	}
 
+	public Integer getCurrentPetCount() {
+		return currentPetCount;
+	}
+
+	public void setCurrentPetCount(Integer currentPetCount) {
+		this.currentPetCount = currentPetCount;
+	}
+
+	public Integer getHoursAlone() {
+		return hoursAlone;
+	}
+
+	public void setHoursAlone(Integer hoursAlone) {
+		this.hoursAlone = hoursAlone;
+	}
+
+	public Integer getHouseholdMemberCount() {
+		return householdMemberCount;
+	}
+
+	public void setHouseholdMemberCount(Integer householdMemberCount) {
+		this.householdMemberCount = householdMemberCount;
+	}
+
+	public Integer getPreviousPetCount() {
+		return previousPetCount;
+	}
+
+	public void setPreviousPetCount(Integer previousPetCount) {
+		this.previousPetCount = previousPetCount;
+	}
+
 	public List<Integer> getChildAges() {
 		return childAges;
 	}
 
 	public void setChildAges(List<Integer> childAges) {
 		this.childAges = childAges;
+	}
+
+	public List<Integer> getHouseholdMemberAges() {
+		return householdMemberAges;
+	}
+
+	public void setHouseholdMemberAges(List<Integer> householdMemberAges) {
+		this.householdMemberAges = householdMemberAges;
+	}
+
+	public List<PreviousPet> getPreviousPets() {
+		return previousPets;
+	}
+
+	public void setPreviousPets(List<PreviousPet> previousPets) {
+		this.previousPets = previousPets;
+	}
+
+	public List<Vet> getVets() {
+		return vets;
+	}
+
+	public void setVets(List<Vet> vets) {
+		this.vets = vets;
+	}
+
+	public List<String> getWhyWantPet() {
+		return whyWantPet;
+	}
+
+	public void setWhyWantPet(List<String> whyWantPet) {
+		this.whyWantPet = whyWantPet;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
 	}
 
 	public String getCaretaker() {
@@ -216,12 +232,216 @@ public class AdoptionApplication implements Serializable {
 		this.caretaker = caretaker;
 	}
 
-	public List<PreviousPet> getPreviousPets() {
-		return previousPets;
+	public String getCity() {
+		return city;
 	}
 
-	public void setPreviousPets(List<PreviousPet> previousPetRecord) {
-		this.previousPets = previousPetRecord;
+	public void setCity(String city) {
+		this.city = city;
 	}
 
+	public String getDaytimeLocation() {
+		return daytimeLocation;
+	}
+
+	public void setDaytimeLocation(String daytimeLocation) {
+		this.daytimeLocation = daytimeLocation;
+	}
+
+	public String getDestructiveBehavior() {
+		return destructiveBehavior;
+	}
+
+	public void setDestructiveBehavior(String destructiveBehavior) {
+		this.destructiveBehavior = destructiveBehavior;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmailConfirm() {
+		return emailConfirm;
+	}
+
+	public void setEmailConfirm(String emailConfirm) {
+		this.emailConfirm = emailConfirm;
+	}
+
+	public String getFailedAdoptionReason() {
+		return failedAdoptionReason;
+	}
+
+	public void setFailedAdoptionReason(String failedAdoptionReason) {
+		this.failedAdoptionReason = failedAdoptionReason;
+	}
+
+	public String getFamilyNotAwareReason() {
+		return familyNotAwareReason;
+	}
+
+	public void setFamilyNotAwareReason(String familyNotAwareReason) {
+		this.familyNotAwareReason = familyNotAwareReason;
+	}
+
+	public String getFencedYard() {
+		return fencedYard;
+	}
+
+	public void setFencedYard(String fencedYard) {
+		this.fencedYard = fencedYard;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getGiveUpSituation() {
+		return giveUpSituation;
+	}
+
+	public void setGiveUpSituation(String giveUpSituation) {
+		this.giveUpSituation = giveUpSituation;
+	}
+
+	public String getHomePhone() {
+		return homePhone;
+	}
+
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+
+	public String getHowContainPet() {
+		return howContainPet;
+	}
+
+	public void setHowContainPet(String howContainPet) {
+		this.howContainPet = howContainPet;
+	}
+
+	public String getIdealPet() {
+		return idealPet;
+	}
+
+	public void setIdealPet(String idealPet) {
+		this.idealPet = idealPet;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLivingSituation() {
+		return livingSituation;
+	}
+
+	public void setLivingSituation(String livingSituation) {
+		this.livingSituation = livingSituation;
+	}
+
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+
+	public String getMonthsLived() {
+		return monthsLived;
+	}
+
+	public void setMonthsLived(String monthsLived) {
+		this.monthsLived = monthsLived;
+	}
+
+	public String getMovingPlan() {
+		return movingPlan;
+	}
+
+	public void setMovingPlan(String movingPlan) {
+		this.movingPlan = movingPlan;
+	}
+
+	public String getPetLivingLocation() {
+		return petLivingLocation;
+	}
+
+	public void setPetLivingLocation(String petLivingLocation) {
+		this.petLivingLocation = petLivingLocation;
+	}
+
+	public String getPlanToDeclaw() {
+		return planToDeclaw;
+	}
+
+	public void setPlanToDeclaw(String planToDeclaw) {
+		this.planToDeclaw = planToDeclaw;
+	}
+
+	public String getSleepLocation() {
+		return sleepLocation;
+	}
+
+	public void setSleepLocation(String sleepLocation) {
+		this.sleepLocation = sleepLocation;
+	}
+
+	public String getTimeSearching() {
+		return timeSearching;
+	}
+
+	public void setTimeSearching(String timeSearching) {
+		this.timeSearching = timeSearching;
+	}
+
+	public String getWhereApplied() {
+		return whereApplied;
+	}
+
+	public void setWhereApplied(String whereApplied) {
+		this.whereApplied = whereApplied;
+	}
+
+	public String getWillingToInstallFence() {
+		return willingToInstallFence;
+	}
+
+	public void setWillingToInstallFence(String willingToInstallFence) {
+		this.willingToInstallFence = willingToInstallFence;
+	}
+
+	public String getYearsLived() {
+		return yearsLived;
+	}
+
+	public void setYearsLived(String yearsLived) {
+		this.yearsLived = yearsLived;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	@Override
+	public String toString() {
+		return OutputUtils.toString(this);
+	}
 }
