81191c9089b5fae88cf1eee9b627129a6d41f84a
skronek <keithskronek@gmail.com>
2012-12-17 02:13:59 -0500

    added request mappings for support sub-pages

--- src/main/java/org/gaar/web/controller/mvc/SupportController.java
+++ src/main/java/org/gaar/web/controller/mvc/SupportController.java
@@ -18,4 +18,19 @@ public class SupportController {
 	public String doDefault(Model model) {
 		return View.support.name();
 	}
+
+	@RequestMapping("kroger")
+	public String onKroger() {
+		return View.kroger.name();
+	}
+
+	@RequestMapping("search-shop-earn")
+	public String onSearchShopEarn() {
+		return View.search_shop_earn.name();
+	}
+
+	@RequestMapping("wishlist")
+	public String onWishlist() {
+		return View.wish_list.name();
+	}
 }
