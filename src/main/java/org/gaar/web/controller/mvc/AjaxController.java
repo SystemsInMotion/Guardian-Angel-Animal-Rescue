58daffd20d92a778f280f14ba4b453b303b6cb82
skronek <keithskronek@gmail.com>
2013-03-12 13:36:36 -0400

    simplified the call the getCats() and getDogs()

--- src/main/java/org/gaar/web/controller/mvc/AjaxController.java
+++ src/main/java/org/gaar/web/controller/mvc/AjaxController.java
@@ -24,14 +24,14 @@ public class AjaxController {
 
 	@RequestMapping("cat-carousel")
 	public String catCarousel(Model model) {
-		final List<PetfinderPetRecord> cats = this.petFinderService.shelterCats(null, null, null, null, null);
+		final List<PetfinderPetRecord> cats = this.petFinderService.shelterCats();
 		model.addAttribute("cats", cats);
 		return View.cat_carousel.name();
 	}
 
 	@RequestMapping("dog-carousel")
 	public String dogCarousel(Model model) {
-		final List<PetfinderPetRecord> dogs = this.petFinderService.shelterDogs(null, null, null, null, null);
+		final List<PetfinderPetRecord> dogs = this.petFinderService.shelterDogs();
 		model.addAttribute("dogs", dogs);
 		return View.dog_carousel.name();
 	}
